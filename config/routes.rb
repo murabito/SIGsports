7b2f5a32a494491fa8e3e8fd9a08cf2d0334a5a3
Christine Murabito <christine.murabito@gmail.com>
2016-06-02 11:14:00 -0400

    Add route for football index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :sports
+  # resources :sports
   resources :athletes
+
+  get 'football', to: 'sports#football'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
