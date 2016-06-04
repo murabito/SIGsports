f62c6d3e0ae14952567cbdd940c96bccda23b19f
Christine Murabito <christine.murabito@gmail.com>
2016-06-04 16:04:52 -0400

    Add route for about us page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'football', to: 'sports#football'
 
+  get '/about', to: 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
