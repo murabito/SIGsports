515a31f2afabe78c86aee345299e962a4c7908f7
Christine Murabito <christine.murabito@gmail.com>
2016-06-05 10:42:56 -0400

    Add controller methods for retrieving athletes for womens and mens basketball

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,6 +1,17 @@
 class SportsController < ApplicationController
   
+  def mens_basketball
+    @football_athletes = Athlete.where(sport: 0)
+    @sport = 0
+  end
+  
+  def womens_basketball
+    @football_athletes = Athlete.where(sport: 1)
+    @sport = 1
+  end
+
   def football
     @football_athletes = Athlete.where(sport: 2)
+    @sport = 2
   end
 end
