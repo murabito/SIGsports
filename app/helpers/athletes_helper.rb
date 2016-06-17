01cf290b46c33a03ff2be3cd2098d4c5e18ab38c
Christine Murabito <christine.murabito@gmail.com>
2016-06-17 12:05:39 -0400

    Add helper method to embed youtube video

--- app/helpers/athletes_helper.rb
+++ app/helpers/athletes_helper.rb
@@ -1,2 +1,15 @@
 module AthletesHelper
+  def embed_youtube(youtube_url)
+    if youtube_url.match('feature=youtu.be')
+      youtube_url = youtube_url.split("&feature=youtu.be").first
+    end
+
+    if (youtube_url.match('youtube') && !youtube_url.match('&list='))
+      youtube_id = youtube_url.split("=").last
+    elsif (youtube_url.match('youtu.be') && !youtube_url.match('&list='))
+      youtube_id = youtube_url.split("/").last
+    end
+
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
 end
