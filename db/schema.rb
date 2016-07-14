541f07ca27712039c5c1aa66b2fe83a20e9952c4
Christine Murabito <christine.murabito@gmail.com>
2016-07-14 18:08:17 -0400

    Add photo attachment to athlete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615210103) do
+ActiveRecord::Schema.define(version: 20160714220707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20160615210103) do
     t.string   "video_url"
     t.integer  "sport_id"
     t.string   "american_team_name"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "athletes", ["sport_id"], name: "index_athletes_on_sport_id", using: :btree
