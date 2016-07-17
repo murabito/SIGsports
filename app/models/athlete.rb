90832ecea59f56d5b367439c467187627e620704
Christine Murabito <christine.murabito@gmail.com>
2016-07-17 17:24:56 -0400

    Add filters to sport pages

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -5,5 +5,5 @@ class Athlete < ActiveRecord::Base
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
-  scope :available, -> { where(:is_available => true) }
+  scope :available, -> { where(is_available: true) }
 end
