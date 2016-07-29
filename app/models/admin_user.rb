ba3c8fd3cf8623ef51f90dd20c05a0c51ad9c389
Christine Murabito <christine.murabito@gmail.com>
2016-07-29 14:54:33 -0400

    Add timeout for admin authentication

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,5 +2,5 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :timeoutable, :trackable, :validatable
 end
