da085baef8e0eb45e550475e0dd427d7f68c86c1
Christine Murabito <christine.murabito@gmail.com>
2016-11-29 14:08:56 -0500

    Determine sport from passed parameter for displaying proper sidebar

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,5 +1,19 @@
 class TeamMembersController < ApplicationController
   def show
+    @sport = find_sport(params[:sport])
     @team_member = TeamMember.find(params[:id])
+  end
+
+  def find_sport(sport_param)
+    if sport_param == 'womens-basketball'
+      sport = Sport.find(3)
+      return sport
+    elsif sport_param == 'mens-basketball'
+      sport = Sport.find(4)
+      return sport
+    elsif  sport_param == 'football'
+      sport = Sport.find(2)
+      return sport
+    end
   end
 end
